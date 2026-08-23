e00336ef42f30cfd6dddbb000f3f915458455eee
Edilberto Calderón T <edilbertocalderon.idetp@gmail.com>
2026-08-22 22:12:30 -0500

    CONFIG:Creando estructura de proyecto con plugin arquitectura limpia/hexagonal de bancolombia

---
+++ applications/app-service/src/main/java/co/com/franquicias/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.franquicias.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.franquicias.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
